80c7053056a237d3c0a77db0becf9974040a172c
Sushant <sushant.deltaforce@gmail.com>
2018-11-13 17:30:07 -0700

    26th Oct, 2018

--- SML_A3_GMM.py
+++ SML_A3_GMM.py
@@ -1,1 +1,15 @@
 __author__ = 'Sushant'
+
+import csv
+
+import numpy as np
+
+
+with open(r"C:\Users\Sushant\Desktop\ASU Courses\CSE575 - Statistical Machine Learning\Assignment - 3\audioData.csv", 'r') \
+        as fp:
+    data_iter = csv.reader(fp, delimiter=',', quotechar='"')
+    data = [data for data in data_iter]
+
+data_array = np.asarray(data)
+print(data_array.shape)
+print("KMeans")
--- SML_A3_Kmeans.py
+++ SML_A3_Kmeans.py
@@ -5,8 +5,10 @@ import random
 
 import numpy as np
 from numpy import linalg as LA
+from matplotlib import pyplot
 
 
+# READING THE FILE DATA
 with open(r"C:\Users\Sushant\Desktop\ASU Courses\CSE575 - Statistical Machine Learning\Assignment - 3\audioData.csv", 'r') \
         as fp:
     data_iter = csv.reader(fp, delimiter=',', quotechar='"')
@@ -18,20 +20,14 @@ Kmeans = np.zeros(10)
 print("KMeans")
 
 # K-Means Algorithm
-
 def Kmeans(data, k):
 
     delta = 999999
     K = np.zeros((10, 13))
-
-    # K = np.random.rand(10, 13)   # Cluster means
-    # K = np.zeros((10, 13))
-    # print("K:", K)
     cond = np.zeros((10, 1))
-    print(cond[0:k], np.count_nonzero(cond[0:k]), k)
 
     while (K[0:k].size - np.count_nonzero(K[0:k])) != 0:
-        print("Restart KMeans: ", K[0:k].size, np.count_nonzero(K[0:k]))
+        # print("Restart KMeans: ", K[0:k].size, np.count_nonzero(K[0:k]))
 
         # Initializing the means
         for i in range(k):
@@ -44,29 +40,28 @@ def Kmeans(data, k):
         while not np.array_equal(k_count_old, k_count):
             k_count_old = k_count
             k_sum = np.zeros((10, 13))
-            k_count = np.empty((10, 1))
             temp = np.zeros((10, 13))
             k_count = np.zeros((10, 1))
+            cost = 0
             for i in range(data.shape[0]):   # Run it for all points
-                # print("DATA: ", data[i])
                 temp[0:k] = K[0:k] - np.tile(data.astype('float')[i], (k, 1))    # Distance from each mean
                 t = LA.norm(temp, axis=1)
-                # print("T", t)
                 k_index = np.argmin(t[0:k])
+                cost += np.power(t[k_index], 2)
                 k_sum[k_index] += data[i].astype('float')
                 k_count[k_index] += 1
-                # print("K_Index: ", k_index, "\nK_SUM: ", k_sum, "\nK_Count: ", k_count)
-                # print("K_Index: ", k_index)
-            # print("Iteration Complete")
             K = np.divide(k_sum, k_count, out=np.zeros_like(k_sum), where=k_count != 0)
-            # print("Final K_Sum: ", k_sum, "\nFinal K_Count: ", k_count, "\nFinal K_div: ", k_ncent)
-            # print("Final K_Count: ", k_count, "\nK_mean: ", k_ncent.shape)
-            # print("Final K_Count: ", k_count[0:k])
-            # print("Final Mean: ", K[0:k])
         k_count[0:k].sort(axis=0)
-        print("Final K_Count: ", k_count[0:k])
-
-
+        # print("Final K_Count: ", k_count[0:k])
         cond = LA.norm(K[0:k], axis=1)
-        print(cond)
-Kmeans(data_array, 7)
+        # print(cond)
+    print("K: ", k, "COST: ", cost)
+    return cost
+
+# MAIN FUNCTION
+temp = []
+for i in range(2, 11):
+    temp.append(Kmeans(data_array, i))
+print("TEMP: ", temp)
+pyplot.plot([i for i in range(2, 11)], temp)
+pyplot.show()
